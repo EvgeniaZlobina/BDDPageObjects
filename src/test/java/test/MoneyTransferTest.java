35f9f3c02d26c6169444616b3c74c8ebc6a0791a
EvgeniaZlobina <ZEA_6590@mail.ru>
2023-08-23 12:34:42 +0500

    Negative test

--- src/test/java/test/MoneyTransferTest.java
+++ src/test/java/test/MoneyTransferTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import page.DashboardPage;
 import page.LoginPage;
+
 import static com.codeborne.selenide.Selenide.open;
 import static data.DataHelper.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoneyTransferTest {
     DashboardPage dashboardPage;
+
     @BeforeEach
     void setup() {
         var loginPage = open("http://Localhost:9999", LoginPage.class);
@@ -18,6 +20,7 @@ public class MoneyTransferTest {
         var verificationCode = getVerificationCode();
         dashboardPage = verificationPage.validVerify(verificationCode);
     }
+
     @Test
     void shouldTransferFromFirstToSecond() {
         var firstCardInfo = getFirstCardInfo();
@@ -33,5 +36,21 @@ public class MoneyTransferTest {
         var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
         assertEquals(expectedDBalanceFirstCard, actualBalanceFirstCard);
         assertEquals(expectedDBalanceSecondCard, actualBalanceSecondCard);
+    }
+
+    @Test
+    void shouldGetErrormessageIfAbountMore() {
+        var firstCardInfo = getFirstCardInfo();
+        var secondCardInfo = getSecondCardInfo();
+        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+        var amount = generateInValidAmount(secondCardBalance);
+        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+        transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
+        transferPage.findErrorMessage("Выполнена попытка перевода суммы, превышающей остаток на карте списания");
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(firstCardBalance, actualBalanceFirstCard);
+        assertEquals(secondCardBalance, actualBalanceSecondCard);
     }
 }
